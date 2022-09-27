6ab5f24ab7cd71bba76519076e01ed0c9b2daaf5
1nyeong <inyeong0726@gmail.com>
2022-09-27 17:46:18 +0900

    springboot 1

---
+++ springboot/mvc-20220927-h/src/main/java/com/study/mvc20220927h/Mvc20220927HApplication.java
@@ -0,0 +1,13 @@
+package com.study.mvc20220927h;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mvc20220927HApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mvc20220927HApplication.class, args);
+	}
+
+}
