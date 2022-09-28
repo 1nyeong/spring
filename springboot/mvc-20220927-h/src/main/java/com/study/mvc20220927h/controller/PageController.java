a0750353ed766604eb5ac2fab1ba38dc7f22a6d9
1nyeong <inyeong0726@gmail.com>
2022-09-28 17:03:15 +0900

    4

--- springboot/mvc-20220927-h/src/main/java/com/study/mvc20220927h/controller/PageController.java
+++ springboot/mvc-20220927-h/src/main/java/com/study/mvc20220927h/controller/PageController.java
@@ -3,12 +3,24 @@ package com.study.mvc20220927h.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class PageController {
 
     @GetMapping("/test1")
-    public  String test1(){
+    public String test1(){
+        return "test_page1";
+    }
+    @GetMapping("/test2")
+    public String test2(){
+        System.out.println("test2");
+        return "test/test_page2";
+    }
+
+    @ResponseBody
+    @GetMapping("/test3")
+    public String test3(){
         return "test_page1";
     }
 }
