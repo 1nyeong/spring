5b840e31b02c068c9b820aeef713ce2222886479
1nyeong <inyeong0726@gmail.com>
2022-09-28 17:43:23 +0900

    5

--- springboot/mvc-20220927-h/src/main/java/com/study/mvc20220927h/controller/PageController.java
+++ springboot/mvc-20220927-h/src/main/java/com/study/mvc20220927h/controller/PageController.java
@@ -1,9 +1,11 @@
 package com.study.mvc20220927h.controller;
 
 
+import com.study.mvc20220927h.dto.TestReqDto2;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class PageController {
@@ -18,9 +20,26 @@ public class PageController {
         return "test/test_page2";
     }
 
-    @ResponseBody
     @GetMapping("/test3")
-    public String test3(){
-        return "test_page1";
+    public String test3(Model model, String strData, @RequestParam String name){
+        System.out.println(strData);
+        System.out.println(name);
+
+        model.addAttribute("data", strData);
+        model.addAttribute("name", name);
+        return "test/test_page3";
+    }
+
+    @GetMapping("/test4")
+    public String test4(Model model, TestReqDto2 testReqDto){
+        System.out.println(testReqDto.getStrData());
+        System.out.println(testReqDto.getName());
+        System.out.println(testReqDto.getNumber());
+
+        model.addAttribute("data", testReqDto.getStrData());
+        model.addAttribute("name", testReqDto.getName());
+        model.addAttribute("number", testReqDto.getNumber());
+
+        return "test/test_page3";
     }
 }
---
+++ springboot/mvc-20220927-h/src/main/java/com/study/mvc20220927h/dto/TestReqDto2.java
@@ -0,0 +1,13 @@
+package com.study.mvc20220927h.dto;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+
+public class TestReqDto2 {
+    private String strData;
+    private String name;
+    private int number;
+}
